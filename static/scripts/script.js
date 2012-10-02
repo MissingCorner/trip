96271f2bf07291f574c5905f184b85d757c62906
Yoshi <vietthang@dynabyte.vn>
2012-10-02 21:28:30 +0700

    Merge commit 'dc89933a9bdddc005dca4430c71ee6c16ff5e74c' into develop
    Conflicts:
    	static/stylesheets/general.less
    	static/stylesheets/iphone.csss

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -68,6 +68,58 @@
 // });
 
 (function($){
+  var touch = {}, touchTimeout;
+
+  function parentIfText(node){
+    return 'tagName' in node ? node : node.parentNode;
+  }
+
+  function swipeDirection(x1, x2, y1, y2){
+    var xDelta = Math.abs(x1 - x2), yDelta = Math.abs(y1 - y2);
+    if (xDelta >= yDelta) {
+      return (x1 - x2 > 0 ? 'Left' : 'Right');
+    } else {
+      return (y1 - y2 > 0 ? 'Up' : 'Down');
+    }
+  }
+
+  $(document).ready(function(){
+    $(document.body).bind('touchstart', function(e){
+      var now = Date.now(), delta = now - (touch.last || now);
+      touch.target = parentIfText(e.originalEvent.touches[0].target);
+      touchTimeout && clearTimeout(touchTimeout);
+      touch.x1 = e.originalEvent.touches[0].pageX;
+      touch.y1 = e.originalEvent.touches[0].pageY;
+      if (delta > 0 && delta <= 250) touch.isDoubleTap = true;
+      touch.last = now;
+    }).bind('touchmove', function(e){
+      touch.x2 = e.originalEvent.touches[0].pageX;
+      touch.y2 = e.originalEvent.touches[0].pageY;
+    }).bind('touchend', function(e){
+      if (touch.isDoubleTap) {
+        $(touch.target).trigger('doubleTap');
+        touch = {};
+      } else if (touch.x2 > 0 || touch.y2 > 0) {
+        (Math.abs(touch.x1 - touch.x2) > 30 || Math.abs(touch.y1 - touch.y2) > 30)  &&
+          $(touch.target).trigger('swipe') &&
+          $(touch.target).trigger('swipe' + (swipeDirection(touch.x1, touch.x2, touch.y1, touch.y2)));
+        touch.x1 = touch.x2 = touch.y1 = touch.y2 = touch.last = 0;
+      } else if ('last' in touch) {
+        touchTimeout = setTimeout(function(){
+          touchTimeout = null;
+          $(touch.target).trigger('tap')
+          touch = {};
+        }, 250);
+      }
+    }).bind('touchcancel', function(){ touch = {} });
+  });
+
+  ['swipe', 'swipeLeft', 'swipeRight', 'swipeUp', 'swipeDown', 'doubleTap', 'tap'].forEach(function(m){
+    $.fn[m] = function(callback){ return this.bind(m, callback) }
+  });
+})(jQuery);
+
+(function($){
   $(document).ready(function(){
     var lastX;
     var lastY;
@@ -83,7 +135,7 @@
       $('.cactive').removeClass('cactive');
     }
 
-    $('#add-button').bind('click', function(e){
+    $('#add-button').bind('tap', function(e){
       console.log('add button');
       if(!isGhostClick(e)){
         if($(this).hasClass('cactive')) {
@@ -96,7 +148,7 @@
       return false;
     });
 
-    $('.magic-content-wrapper').bind('click', function(e){
+    $('.magic-content-wrapper').bind('tap', function(e){
       if(!isGhostClick(e)){
         $('.magic-content-wrapper').removeClass('cactive');
         $(this).addClass('cactive');
@@ -105,12 +157,32 @@
       return false;
     });
 
-    $('body').bind('click', function(e){
-      console.log('body');
+    $('body').bind('tap', function(e){
       if(!isGhostClick(e)){
         removeAllCactive();
       }
     });
 
+  });
+})(jQuery);
+
+(function($){
+  $(document).ready(function(){
+    $('#agenda .loc-list').on('swipeRight', 'li.active', function(){
+      var $this = $(this);
+      console.log(this);
+      $this.removeClass('active');
+      $this.parent().find('li.ready').removeClass('ready').addClass('active');
+      console.log('blah');
+    });
+
+    $('.loc-list').iosSlider({
+      elasticPullResistance : 0.3,
+      snapToChildren : true,
+      onSlideChange : function(options){
+        var ele = options.currentSlideObject[0];
+        // var img =
+      }
+    });
   });
 })(jQuery);
